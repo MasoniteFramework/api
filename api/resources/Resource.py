87cc87b378a7a73b992cd1e2a5b7bf906ed12b30
Joseph Mancuso <idmann509@gmail.com>
2018-09-22 19:45:25 -0400

    added test

--- api/resources/Resource.py
+++ api/resources/Resource.py
@@ -4,8 +4,8 @@ class Resource:
     
     model = None
 
-    def __init__(self):
-        self.route_url = None
+    def __init__(self, url=None):
+        self.route_url = url
     
     def get_response(self):
         """Gets the response that should be returned from this resource
@@ -20,10 +20,46 @@ class Resource:
         """
         pass
     
-    def compile_route_to_regex(self):
+    def load_request(self, request):
+        self.request = request
+        return self
+    
+    def compile_route_to_regex(self, router):
         """Compiles this resource url to a regex pattern
         """
-        pass
+
+        # Split the route
+        split_given_route = self.route_url.split('/')
+        # compile the provided url into regex
+        url_list = []
+        regex = '^'
+        for regex_route in split_given_route:
+            if '@' in regex_route:
+                if ':' in regex_route:
+                    try:
+                        regex += router.route_compilers[regex_route.split(':')[
+                            1]]
+                    except KeyError:
+                        raise InvalidRouteCompileException(
+                            'Route compiler "{}" is not an available route compiler. '
+                            'Verify you spelled it correctly or that you have added it using the compile() method.'.format(
+                                regex_route.split(':')[1])
+                        )
+                else:
+                    regex += router.route_compilers['default']
+
+                regex += r'\/'
+
+                # append the variable name passed @(variable):int to a list
+                url_list.append(
+                    regex_route.replace('@', '').split(':')[0]
+                )
+            else:
+                regex += regex_route + r'\/'
+
+        router.url_list = url_list
+        regex += '$'
+        return regex
 
     def create(self): 
         """Logic to create data from a given model
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,5 +1,49 @@
 from api.resources import Resource
 from tests.models.User import User
+from masonite.app import App
+from masonite.providers import RouteProvider
+from masonite.testsuite import TestSuite, generate_wsgi
+from masonite.routes import Route
+from masonite.request import Request
+from masonite.view import View
 
 class ResourceTest(Resource):
     model = User
+    method_type = 'GET'
+
+    def get_response(self):
+        return 'test'
+
+class Application:
+    DEBUG = True
+
+class TestResource:
+    
+    def setup_method(self):
+        self.app = App()
+        self.app.bind('Container', self.app)
+        self.app.bind('Environ', generate_wsgi())
+        self.app.bind('Application', Application)
+        self.app.bind('WebRoutes', [])
+        self.app.bind('Route', Route(self.app.make('Environ')))
+        self.app.bind('Request', Request(
+            self.app.make('Environ')).load_app(self.app))
+        self.app.bind('Headers', [])
+        self.app.bind('StatusCode', '404 Not Found')
+        self.app.bind('HttpMiddleware', [])
+        view = View(self.app)
+        self.app.bind('ViewClass', view)
+        self.app.bind('View', view.render)
+        self.provider = RouteProvider()
+        self.provider.app = self.app
+        
+    def test_resource_can_return_response_as_route(self):
+        self.app.make('Route').url = '/view'
+        self.app.bind('WebRoutes', [ResourceTest('/view')])
+
+        self.provider.boot(
+            self.app.make('Route'),
+            self.app.make('Request')
+        )
+
+        assert self.app.make('Response') == 'test'
