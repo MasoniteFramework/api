296af8a411f345f83b918d885457db53f9201c56
Joseph Mancuso <idmann509@gmail.com>
2018-09-22 19:51:25 -0400

    added masonite project to package

---
+++ config/database.py
@@ -0,0 +1,49 @@
+""" Database Settings """
+
+import os
+
+from masonite.environment import LoadEnvironment
+from orator import DatabaseManager, Model
+
+"""
+|--------------------------------------------------------------------------
+| Load Environment Variables
+|--------------------------------------------------------------------------
+|
+| Loads in the environment variables when this page is imported.
+|
+"""
+
+LoadEnvironment()
+
+"""
+|--------------------------------------------------------------------------
+| Database Settings
+|--------------------------------------------------------------------------
+|
+| Set connection database settings here as a dictionary. Follow the
+| format below to create additional connection settings.
+|
+| @see Orator migrations documentation for more info
+|
+"""
+
+DATABASES = {
+    'default': os.environ.get('DB_DRIVER'),
+    'sqlite': {
+        'driver': 'sqlite',
+        'database': os.environ.get('DB_DATABASE'),
+        'prefix': ''
+    },
+    os.environ.get('DB_DRIVER'): {
+        'driver': os.environ.get('DB_DRIVER'),
+        'host': os.environ.get('DB_HOST'),
+        'database': os.environ.get('DB_DATABASE'),
+        'user': os.environ.get('DB_USERNAME'),
+        'password': os.environ.get('DB_PASSWORD'),
+        'prefix': ''
+    }
+}
+
+DB = DatabaseManager(DATABASES)
+Model.set_connection_resolver(DB)
