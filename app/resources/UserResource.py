1d94cf89adf3ced5f5c751069a2d1dfb5c9d790c
Joseph Mancuso <idmann509@gmail.com>
2018-09-23 14:31:20 -0400

    removed code comments

--- app/resources/UserResource.py
+++ app/resources/UserResource.py
@@ -12,13 +12,3 @@ class UserResource(Resource, JSONSerializer, JWTAuthentication, PermissionScopes
     without = ['password', 'plan_id', 'remember_token']
     scopes = ['user:read']
 
-    # def authenticate(self, request: Request):
-    #     if request.input('token'):
-    #         token = request.input('token')
-    #     elif request.header('HTTP_AUTHORIZATION'):
-    #         token = request.header('HTTP_AUTHORIZATION').replace('Basic ', '')
-    #     else:
-    #         raise NoApiTokenFound
-        
-    #     if not token == '1234':
-    #         raise ApiNotAuthenticated
