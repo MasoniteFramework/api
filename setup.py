1da030d283df0aff9afdf9414ad000e02da54f95
Joseph Mancuso <idmann509@gmail.com>
2018-09-23 12:25:34 -0400

    fixed masonite dependency

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['masonite==2.1.0b1.post1'],
+    install_requires=['masonite'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
