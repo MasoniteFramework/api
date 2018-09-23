a9e91c5feff580ca551c0c676a5a89d159deadf6
Joseph Mancuso <idmann509@gmail.com>
2018-09-23 12:15:50 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -64,8 +64,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'https://github.com/masoniteframework/core/archive/develop.zip'],
+    install_requires=['masonite==2.1.0b1.post1'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
