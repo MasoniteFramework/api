7b99fcdd317df7a83c9d181e9b739daa4705be6a
Joseph Mancuso <idmann509@gmail.com>
2018-09-23 10:00:09 -0400

    better token authentication

--- api/controllers/TokenController.py
+++ api/controllers/TokenController.py
@@ -1,6 +1,7 @@
 from masonite.auth import Sign
+from masonite.helpers.misc import random_string
 
 class TokenController:
 
     def token(self):
-        return {'token': Sign().sign('12345')}   
+        return {'token': Sign().sign(random_string(10))}   
