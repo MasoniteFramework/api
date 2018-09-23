26a8fb3a5fc807b4ea522eb4fd3416b2e5afdd47
Joseph Mancuso <idmann509@gmail.com>
2018-09-23 14:31:39 -0400

    cleaned up file

--- app/resources/UserResource.py
+++ app/resources/UserResource.py
@@ -1,14 +1,15 @@
+from masonite.request import Request
+
+from api.authentication import (JWTAuthentication, PermissionScopes,
+                                TokenAuthentication)
+from api.exceptions import ApiNotAuthenticated, NoApiTokenFound
 from api.resources import Resource
 from api.serializers import JSONSerializer
 from app.User import User
-from api.exceptions import ApiNotAuthenticated, NoApiTokenFound
-from masonite.request import Request
-from api.authentication import TokenAuthentication, JWTAuthentication, PermissionScopes
 
 
 class UserResource(Resource, JSONSerializer, JWTAuthentication, PermissionScopes):
-    
+
     model = User
     without = ['password', 'plan_id', 'remember_token']
     scopes = ['user:read']
-
