b51cdf0617843e6b4bab7f8565018e8d65709da1
Joseph Mancuso <idmann509@gmail.com>
2019-11-30 11:06:27 -0500

    Moved to Beta

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.0.0',
+    version='3.0.0b1',
     package_dir={'': 'src'},
 
     description='Masonite API Package',
@@ -28,7 +28,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
